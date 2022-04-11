23c8f6fe19f82445ce671391c9e499cdbd57bc9c
Noel <noel.huberto@gmail.com>
2022-04-11 18:56:49 +0200

    Login Controller done

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -1,12 +1,17 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * Login Controller
+ */
+@Slf4j
 @Controller
 @RequestMapping("app")
 public class LoginController {
@@ -14,23 +19,38 @@ public class LoginController {
     @Autowired
     private UserRepository userRepository;
 
+    /**
+     * Get login
+     * @return login
+     */
     @GetMapping("login")
     public ModelAndView login() {
+        log.info("GET login");
         ModelAndView mav = new ModelAndView();
         mav.setViewName("login");
         return mav;
     }
 
+    /**
+     * Get secure/article-details
+     * @return user/list
+     */
     @GetMapping("secure/article-details")
     public ModelAndView getAllUserArticles() {
+        log.info("GET secure/article-details");
         ModelAndView mav = new ModelAndView();
         mav.addObject("users", userRepository.findAll());
         mav.setViewName("user/list");
         return mav;
     }
 
+    /**
+     * Get error
+     * @return 403
+     */
     @GetMapping("error")
     public ModelAndView error() {
+        log.info("GET error");
         ModelAndView mav = new ModelAndView();
         String errorMessage= "You are not authorized for the requested data.";
         mav.addObject("errorMsg", errorMessage);

851be1a93fb0e1fb75ca54be1236079be09a7873
Noel <noel.huberto@gmail.com>
2022-04-23 15:53:20 +0200

    Constraint updated for password in User

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/User.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/User.java
@@ -3,7 +3,7 @@ package com.nnk.springboot.domain;
 import lombok.Data;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
 
 /**
  * User Entity
@@ -17,8 +17,16 @@ public class User {
     private Integer id;
     @NotBlank(message = "Username is mandatory")
     private String username;
+
     @NotBlank(message = "Password is mandatory")
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[.#?!@$%^&*-]).{8,}$",
+            message = "At least one upper case English letter + "+
+                    "At least one lower case English letter + "+
+                    "At least one digit + "+
+                    "At least one special character + "+
+                    "Minimum eight in length")
     private String password;
+
     @NotBlank(message = "FullName is mandatory")
     private String fullname;
     @NotBlank(message = "Role is mandatory")
